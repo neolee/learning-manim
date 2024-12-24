1e9f46a36ac6048e5c199172f1246563f2d55abb
Neo Lee <neo.lee@gmail.com>
2024-12-24 16:00:14 +0800

    refactor: better naming

--- start.py
+++ start.py
@@ -2,26 +2,27 @@ from manimlib import * # type: ignore
 
 class SquareToCircle(Scene):
     def construct(self):
-        circle = Circle()  # create a circle
-        circle.set_fill(PINK, opacity=0.5)  # set color and transparency
+        circle = Circle()
+        circle.set_fill(BLUE, opacity=0.5)
+        circle.set_stroke(BLUE_E, width=4)
+        square = Square()
 
-        square = Square()  # create a square
-        square.rotate(PI / 4)  # rotate a certain amount
-
-        self.play(Create(square))  # animate the creation of the square
-        self.play(Transform(square, circle))  # interpolate the square into the circle
-        self.play(FadeOut(square))  # fade out animation
-
+        self.play(ShowCreation(square))
+        self.wait()
+        self.play(ReplacementTransform(square, circle))
+        self.wait()
 
 class Anagram(Scene):
     def construct(self):
+        # source = Text("two eleven", height=1)
+        # target = Text("one twelve", height=1)
         source = Text("two eleven", font="Consolas", font_size=90)
         target = Text("one twelve", font="Consolas", font_size=90)
 
         self.play(Write(source))
-        self.wait(0.5)
-        kw = {"run_time": 2, "path_arc": PI / 2}
+        self.wait()
+        kw = {"run_time": 3, "path_arc": PI / 2}
         self.play(TransformMatchingShapes(source, target, **kw))
-        self.wait(0.5)
+        self.wait()
         self.play(TransformMatchingShapes(target, source, **kw))
-        self.wait(0.5)
+        self.wait()
